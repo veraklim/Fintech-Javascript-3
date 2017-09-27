66492249004ea2bdb9c16c024fbe1c54883495e6
Vera Klimina <veraklim>
2017-09-27 22:25:36 +0300

    HW_1

--- 01/tasks.js
+++ 01/tasks.js
@@ -5,7 +5,24 @@
  * '1 и 6.45, -2, но 8, а затем 15, то есть 2.7 и -1028' => { min: -1028, max: 15 }
  */
 function getMinMax(string) {
+  var min = Number.MAX_VALUE;
+  var max = Number.MIN_VALUE;
+  var cnt = 0.0;
+  var str = string.split(' ');
 
+  for (var i = 0; i < str.length; i++) {
+    cnt = parseFloat(str[i]);
+    if (!isNaN(cnt)) {
+      if (cnt < min) {
+        min = cnt;
+      } else if (cnt > max) {
+        max = cnt;
+      }
+    } else {
+      continue;
+    }
+  }
+  return { max, min };
 }
 
 /* ============================================= */
@@ -16,7 +33,11 @@ function getMinMax(string) {
  * @return {number} число под номером х
  */
 function fibonacciSimple(x) {
-  return x;
+  if ( x === 1 || x === 2) {
+    return 1;
+  } else {
+    return fibonacciSimple( x - 1 )+fibonacciSimple( x - 2 );
+  }
 }
 
 /* ============================================= */
@@ -27,8 +48,17 @@ function fibonacciSimple(x) {
  * @param {number} x номер числа
  * @return {number} число под номером х
  */
-function fibonacciWithCache(x) {
-  return x;
+function fibonacciWithCache(x, fibonacci) {
+  fibonacci = fibonacci || {};
+  if ( x === 1 || x === 2) {
+    fibonacci[x] = 1;
+    return fibonacci[x];
+  }
+
+  if (isNaN(fibonacci[x])) {
+    fibonacci[x] = fibonacciWithCache(x - 1, fibonacci) + fibonacciWithCache(x - 2, fibonacci);
+  }
+  return fibonacci[x];
 }
 
 /* ============================================= */
@@ -49,7 +79,35 @@ function fibonacciWithCache(x) {
  * @return {string}
  */
 function printNumbers(max, cols) {
-
+  var line = 0;
+  var str = '';
+  if ((max+1) % cols == 0 ) {
+    line = (max+1) / cols;
+  } else {
+    line = 1 + Math.floor((max + 1) / cols);
+  }
+  for (var i = 0; i < line; i++ ) {
+    for (var j = 0; j < cols; j++) {
+      if (j*line + i < max+1) {
+        if (j>0) {
+          if (j * line + i >= 10){
+            str += ' ';
+          } else {
+            str += '  ';
+          }
+        } else {
+          str += ' ';
+        }
+        str += j*line + i;
+        if (j == cols-1 && i!= line-1) {
+          str += "\n";
+        }
+      } else {
+        break;
+      }
+    }
+  }
+  return str;
 }
 
 /* ============================================= */
@@ -60,7 +118,22 @@ function printNumbers(max, cols) {
  * @return {string}
  */
 function rle(input) {
-
+  var str = '';
+  var crnt = 1;
+  for (var i = 1;input.length >= i ;i++ ) {
+    if (input[i] == input[i - 1]) {
+      crnt += 1;
+    } else {
+      if (crnt > 1) {
+        str += input[i - 1];
+        str += crnt;
+        crnt = 1;
+      } else {
+        str += input[i - 1];
+      }
+    }
+  }
+  return str;
 }
 
 module.exports = {
